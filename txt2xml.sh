9384477dbd707e693347ca8c2b11478838284e98
Zachary P. Brasseaux <zippy1224@gmail.com>
2017-04-27 21:27:41 -0500

    Update txt2xml.sh

--- txt2xml.sh
+++ txt2xml.sh
@@ -1,3 +1,6 @@
+# Credit for this one goes to sgzUk74r3T3BCGmRJ from the Linus Tech Tips Forum.
+# Requires txt2html and tidy be installed, which can be done with "sudo apt-get install txt2xml: and "sudo apt-get install tidy".
+
 for i in *.txt; do txt2html $i | tidy -q -asxml > ${i%.txt}.xml; done
 
 mkdir TextFiles

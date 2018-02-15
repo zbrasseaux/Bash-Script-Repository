d0b2db63a52f4663640b8a2293621c9bce387886
zbrasseaux <zbrasseaux97@gmail.com>
2018-02-15 13:40:10 -0600

    full comic converter

--- comicConv.sh
+++ comicConv.sh
@@ -1,80 +1,139 @@
+cbr()
+{
+	compName=$(basename "$1" .cbr).rar
+	dirName=$(basename "$1" .cbr)
+	pdfName=$(basename "$1" .cbr).pdf
 
-# Zachary Brasseaux (c) 2017
-
-echo "Input the comic book file you wish to convert, then hit [ENTER]"
-
-read inFile
-
-for f in $inFile; do
-	
-	if [ ${f: -4} == ".cbr" ]; then
-	
-		compName=$(basename "$f" .cbr).rar
-		dirName=$(basename "$f" .cbr)
-		pdfName=$(basename "$f" .cbr).pdf
-
-		cp "$f" $rarName
-		unrar x -y -inul $compName
-		convert $dirName/* $pdfName
-		rm -r $dirName
+	cp "$1" $rarName
+	unrar x -y -inul $compName
+	convert $dirName/* $pdfName
+	rm -r $dirName
 		
-		echo "Conversion complete, please enjoy $pdfName"
-	fi
-	
-	if [ ${f: -4} == ".cbz" ]; then
-	
-		compName=$(basename "$f" .cbz).zip
-		dirName=$(basename "$f" .cbz)
-		pdfName=$(basename "$f" .cbz).pdf
+	echo "Conversion complete, please enjoy $pdfName"
+}
 
-		cp "$f" $compName
-		unrar x -y -inul $compName
-		convert $dirName/* $pdfName
-		rm -r $dirName
+cbz()
+{
+	compName=$(basename "$1" .cbz).zip
+	dirName=$(basename "$1" .cbz)
+	pdfName=$(basename "$1" .cbz).pdf
+
+	cp "$1" $compName
+	unrar x -y -inul $compName
+	convert $dirName/* $pdfName
+	rm -r $dirName
 		
-		echo "Conversion complete, please enjoy $pdfName"
-	fi
+	echo "Conversion complete, please enjoy $pdfName"
+}
 
-	if [ ${f: -4} == ".cb7" ]; then
-	
-		compName=$(basename "$f" .cb7).7z
-		dirName=$(basename "$f" .cb7)
-		pdfName=$(basename "$f" .cb7).pdf
+cb7()
+{
+	compName=$(basename "$1" .cb7).7z
+	dirName=$(basename "$1" .cb7)
+	pdfName=$(basename "$1" .cb7).pdf
 
-		cp "$f" $compName
-		p7zip -d $compName
-		convert $dirName/* $pdfName
-		rm -r $dirName
+	cp "$1" $compName
+	p7zip -d $compName
+	convert $dirName/* $pdfName
+	rm -r $dirName
 		
-		echo "Conversion complete, please enjoy $pdfName"
-	fi
+	echo "Conversion complete, please enjoy $pdfName"
+}
 
-	if [ ${f: -4} == ".cbt" ]; then
-	
-		compName=$(basename "$f" .cbt).tar
-		dirName=$(basename "$f" .cbt)
-		pdfName=$(basename "$f" .cbt).pdf
+cbt()
+{
+	compName=$(basename "$1" .cbt).tar
+	dirName=$(basename "$1" .cbt)
+	pdfName=$(basename "$1" .cbt).pdf
 
-		cp "$f" $compName
-		tar xf $compName $dirName
-		convert $dirName/* $pdfName
-		rm -r $dirName
+	cp "$1" $compName
+	tar xf $compName $dirName
+	convert $dirName/* $pdfName
+	rm -r $dirName
 		
-		echo "Conversion complete, please enjoy $pdfName"
-	fi
+	echo "Conversion complete, please enjoy $pdfName"
+}
 
-	if [ ${f: -4} == ".cba" ]; then
-	
-		compName=$(basename "$f" .cba).ace
-		dirName=$(basename "$f" .cba)
-		pdfName=$(basename "$f" .cba).pdf
+cba()
+{
+	compName=$(basename "$1" .cba).ace
+	dirName=$(basename "$1" .cba)
+	pdfName=$(basename "$1" .cba).pdf
 
-		cp "$f" $compName
-		unace e $compName
-		convert $dirName/* $pdfName
-		rm -r $dirName
+	cp "$1" $compName
+	unace e $compName
+	convert $dirName/* $pdfName
+	rm -r $dirName
 		
-		echo "Conversion complete, please enjoy $pdfName"
-	fi
+	echo "Conversion complete, please enjoy $pdfName"
+}
 
+version()
+{
+	echo
+	echo "Comic Book Converter:"
+	echo "version 2.0.0"
+	echo "Written by Zachary Brasseaux"
+	echo
+	exit 0
+}
+
+usage()
+{
+    echo
+    echo 'Usage: ./comicConv.sh [flags/filetype] [input_comic_file]'
+    echo
+    echo "Options:"
+    echo '-v | --version    Displays current version information'
+    echo '-h | --help       Displays a full list of options'
+    echo
+    echo "Options for declaring file type:"
+    echo
+    echo '-z | --cbz'
+    echo '-r | --cbr'
+    echo '-7 | --cb7'
+    echo '-t | --cbt'
+    echo '-a | --cba'
+    echo
+    echo "Required programs:"
+    echo "	image-magic"
+    echo "	unrar"
+    echo "	p7zip"
+    echo "	unace"
+    exit 0
+}
+
+throwError()
+{
+	echo
+	echo "Invalid filetype/flags and/or file specified. Please try again with a valid file."
+	echo " try './comicConv.sh -h' or './comicConv.sh --h' for more [options]"
+	echo
+	exit 1
+}
+
+while [ $1 != "" ]; do
+	case $1 in
+		-v | --version ) 	 	version
+						 	 	;;
+		-h | --help ) 		 	usage
+					  		 	;;
+		-z | --cbz ) 			shift
+								cbz
+							 	;;
+		-r | --cbr ) 			shift
+								cbr
+							 	;;
+		-7 | --cb7 )			shift
+								cb7
+							 	;;
+		-t | --cbt ) 			shift
+								cbt
+							 	;;						 							 
+		-a | --cba ) 			shift
+								cba
+							 	;;
+		* )					 	throwError
+	esac
+		shift
 done
